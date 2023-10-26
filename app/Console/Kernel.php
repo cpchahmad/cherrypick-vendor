1fe3c42a2535dd440d6322891e2e77418176f94a
Zain-Irfan009 <zain.irfan@tetralogicx.com>
2023-10-26 19:02:08 +0500

    commit

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -35,7 +35,7 @@ class Kernel extends ConsoleKernel
         //comment by zain
 //		$schedule->command('update:otp')->everyFiveMinutes();
 		$schedule->command('update:price')->everyMinute()->withoutOverlapping();
-		$schedule->command('products:approve')->everyMinute()->withoutOverlapping();
+//		$schedule->command('products:approve')->everyMinute()->withoutOverlapping();
 		$schedule->command('fetch:jsonproduct')->everySixHours()->withoutOverlapping();
         $schedule->command('update:priceConversionRate')->everyMinute();
     }
